e9bb125d53b65f1f29e4fe63b6927c181e913997
ritikagopran-spec <ritikagopran@gmail.com>
2026-09-22 10:58:57 +0530

    Update digital_app.py

--- digital_app.py
+++ digital_app.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.set_page_config(page_title="AI Resume Builder", page_icon="🚀")
 
 st.title("🚀 AI Digital Resume Builder")
-st.write("GLA University - B.Tech Mini Project")
+st.write("AKTU University - B.Tech Mini Project")
 
-name = st.text_input("Your Name", "Your Name")
+name = st.text_input("Your Name", "ANCHAL")
 role = st.text_input("Role", "B.Tech CSE Student")
-email = st.text_input("Email", "email@gmail.com")
+email = st.text_input("Email", "ritikagopran@gmail.com")
 skills = st.text_area("Skills", "Python, AI, Web Dev")
 about = st.text_area("About You", "I am a passionate student...")
 
